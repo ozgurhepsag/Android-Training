ee5de7fe1298df9c320bcec21aa4ffb0e2632832
Özgür Hepsağ <ozgurhepsag@gmail.com>
2019-03-05 21:00:41 +0300

    Finished FragmentActivityCommunication

--- FragmentActivityCommunication/app/src/main/java/com/example/fragmentactivitycommunication/MainActivity.java
+++ FragmentActivityCommunication/app/src/main/java/com/example/fragmentactivitycommunication/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.fragmentactivitycommunication;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MessageFragment.OnMessageReadListener {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.txt_display_message);
+
+        if(findViewById(R.id.fragment_container) != null){
+
+            if(savedInstanceState != null){
+                return;
+            }
+
+            MessageFragment messageFragment = new MessageFragment();
+
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, messageFragment, null);
+            fragmentTransaction.commit();
+
+        }
+    }
+
+    @Override
+    public void onMessageRead(String message) {
+        textView.setText(message);
     }
 }
--- FragmentActivityCommunication/app/src/main/java/com/example/fragmentactivitycommunication/MessageFragment.java
+++ FragmentActivityCommunication/app/src/main/java/com/example/fragmentactivitycommunication/MessageFragment.java
@@ -1,11 +1,15 @@
 package com.example.fragmentactivitycommunication;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -13,17 +17,52 @@ import android.view.ViewGroup;
  */
 public class MessageFragment extends Fragment {
 
+    private EditText editText;
+    private Button button;
+    OnMessageReadListener messageReadListener;
 
     public MessageFragment() {
         // Required empty public constructor
     }
 
+    public interface OnMessageReadListener{
+
+        public void onMessageRead(String message);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_message, container, false);
+
+        editText = view.findViewById(R.id.text_message);
+        button = view.findViewById(R.id.bn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = editText.getText().toString();
+
+                messageReadListener.onMessageRead(message);
+            }
+        });
+
+        return view;
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        Activity activity = (Activity) context;
+        try {
+            messageReadListener = (OnMessageReadListener) activity;
+        }catch (ClassCastException e){
+
+            throw new ClassCastException(activity.toString() + " must override onMessageRead...");
+        }
+
+
+    }
 }
