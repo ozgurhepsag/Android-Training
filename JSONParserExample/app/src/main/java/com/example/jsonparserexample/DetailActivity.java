3306038ea62a88c305350c34d3ce6b05fd0ce6b6
Özgür Hepsağ <ozgurhepsag@gmail.com>
2019-04-12 23:17:17 +0300

    Add Detail Activity

--- JSONParserExample/app/src/main/java/com/example/jsonparserexample/DetailActivity.java
+++ JSONParserExample/app/src/main/java/com/example/jsonparserexample/DetailActivity.java
@@ -1,7 +1,12 @@
 package com.example.jsonparserexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        String imageUrl = intent.getStringExtra(MainActivity.EXTRA_URL);
+        String creatorName = intent.getStringExtra(MainActivity.EXTRA_CREATOR);
+        int likeCount = intent.getIntExtra(MainActivity.EXTRA_LIKES, 0);
+
+        ImageView imageView = findViewById(R.id.image_view_detail);
+        TextView textViewCreator = findViewById(R.id.text_view_creator_detail);
+        TextView textViewLikes = findViewById(R.id.text_view_like_detail);
+
+        Picasso.get().load(imageUrl).fit().centerInside().into(imageView);
+        textViewCreator.setText(creatorName);
+        textViewLikes.setText("Likes: " + likeCount);
     }
 }
